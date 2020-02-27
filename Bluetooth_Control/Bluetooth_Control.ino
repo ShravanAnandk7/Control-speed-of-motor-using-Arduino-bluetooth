77c666d7cb9f7f7e73984ce357354f39c53b87d4
Shravan Anand Komakula <55278866+ShravanAnandk7@users.noreply.github.com>
2020-02-28 02:23:08 +0530

    Add files via upload

---
+++ Bluetooth_Control/Bluetooth_Control.ino
@@ -0,0 +1,28 @@
+void setup() {
+
+Serial.begin(9600);
+
+pinMode(6,OUTPUT);
+
+pinMode(9,OUTPUT);
+}
+
+void loop() {
+     
+        if(Serial.available()>0){                  
+        int c=Serial.read();
+           c=c-48;   
+        Serial.print("available c = ");
+        Serial.println(c);
+
+        int spd = map( c , 0 , 9 , 0 ,255);
+        Serial.print("Speed spd = ");
+        Serial.println(spd);
+        analogWrite(6,spd);
+        analogWrite(9,0);
+  
+}
+}
+  
+
+
